4d1272cfbc82fc76389711bee7c471412d801e35
Tommie Behrenbeck <behrenbt@gmail.com>
2018-09-20 21:04:08 -0500

    nav and header

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/Header.css";
+
+const Header = () => (
+  <header className="header">
+    <h1>Clicky Game</h1>
+  </header>
+);
+
+export default Header;
