20bd981578122dd17feaedfc30009e17e48a48ea
Tommie Behrenbeck <behrenbt@gmail.com>
2018-09-22 14:47:32 -0500

    restructure card

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import Header from "./components/Header";
-import Section from "./components/Section";
-import Footer from "./components/Footer";
+import React, { Component } from "react";
+import WonderCard from "./components/WonderCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import wonder from "./wonder.json";
+import "./App.css";
 
-const App = () => (
-  <div>
-    <Navbar />
-    <Header />
-    <Section />
-    <Footer />
-  </div>
-);
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    wonder
+  };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        {/* <Title>Clicky Game</Title> */}
+        {this.state.wonder.map(wonder => (
+          <WonderCard id={wonder.id} key={wonder.id} image={wonder.image} />
+        ))}
+      </Wrapper>
+    );
+  }
+}
 
 export default App;
